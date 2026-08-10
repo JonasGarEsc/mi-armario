d6c2391758a1446f830f88b551d519c3633ebff7
JonasGarEsc <jonasge44@gmail.com>
2026-08-10 17:14:11 +0200

    cambio de nombre y profesionalizacion de la app, cambio de logo y nombre a Perletta, gestor de armario y maletas

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Armario Virtual',
-        short_name: 'Armario',
+        short_name: 'Perletta',
         description: 'Gestor de armario y maletas',
         theme_color: '#0f172a',
         background_color: '#ffffff',
